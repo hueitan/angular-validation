a58a1c4ba767b9dd59f3c2d1acb5afa8c8af6225
Huei <huei90@gmail.com>
2014-05-30 16:49:28 +0800

    Update demo code

--- demo/demo.js
+++ demo/demo.js
@@ -1,136 +1,137 @@
-angular.module('myApp', ['validation'])
+(function () {
+    angular.module('myApp', ['validation'])
 
-    // -------------------
-    // config phase
-    // -------------------
-    .config(['$validationProvider', function ($validationProvider) {
+        // -------------------
+        // config phase
+        // -------------------
+        .config(['$validationProvider', function ($validationProvider) {
 
-        var defaultMsg,
-            expression;
+            var defaultMsg,
+                expression;
 
-        /**
-         * Setup a default message for Url
-         */
-        defaultMsg = {
-            url: {
-                error: 'This is a error url given by user',
-                success: 'It\'s Url'
-            }
-        };
-
-        $validationProvider.setDefaultMsg(defaultMsg);
-
-
-        /**
-         * Setup a new Expression and default message
-         * In this example, we setup a IP address Expression and default Message
-         */
-        expression = {
-            ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-        };
-
-        defaultMsg = {
-            ip: {
-                error: 'This isn\'t ip address',
-                success: 'It\'s ip'
-            }
-        };
-
-        $validationProvider.setExpression(expression);
-        $validationProvider.setDefaultMsg(defaultMsg);
-
-        // or we can just setup directly
-        $validationProvider.setDefaultMsg({ ip: { error: 'This no ip', success: 'this ip'}});
-        $validationProvider.setExpression({ ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/ });
-
-        /**
-         * Additions validation
-         */
-        $validationProvider.setExpression({
             /**
-             * @param value , user input
-             * @returns {boolean} true iff valid
+             * Setup a default message for Url
              */
-            huei: function (value) {
-                return value === 'Huei Tan';
-            }
-        });
+            defaultMsg = {
+                url: {
+                    error: 'This is a error url given by user',
+                    success: 'It\'s Url'
+                }
+            };
 
-        $validationProvider.setDefaultMsg({
-            huei: {
-                error: 'This should be Huei Tan',
-                success: 'Thanks!'
-            }
-        });
-    }])
-
-    // -------------------
-    // controller phase
-    // -------------------
-
-    .controller('index', ['$scope', '$injector', function ($scope, $injector) {
-
-        // Injector
-        var $validationProvider = $injector.get('$validation');
+            $validationProvider.setDefaultMsg(defaultMsg);
 
 
-        // Initial Value
-        $scope.form = {
-            requiredCallback: 'required',
-            checkValid: $validationProvider.checkValid,
-            submit: function (form) {
-                $validationProvider.validate(form);
-            },
-            reset: function (form) {
-                $validationProvider.reset(form);
-            }
-        };
+            /**
+             * Setup a new Expression and default message
+             * In this example, we setup a IP address Expression and default Message
+             */
+            expression = {
+                ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+            };
 
-        $scope.form2 = {
-            checkValid: $validationProvider.checkValid,
-            submit: function (form) {
-                $validationProvider.validate(form);
-            },
-            reset: function (form) {
-                $validationProvider.reset(form);
-            }
-        };
+            defaultMsg = {
+                ip: {
+                    error: 'This isn\'t ip address',
+                    success: 'It\'s ip'
+                }
+            };
 
-        $scope.form3 = {
-            checkValid: $validationProvider.checkValid,
-            submit: function (form) {
-                $validationProvider.validate(form)
-                    .success($scope.success)
-                    .error($scope.error);
-            },
-            reset: function (form) {
-                $validationProvider.reset(form);
-            }
-        };
+            $validationProvider.setExpression(expression);
+            $validationProvider.setDefaultMsg(defaultMsg);
 
-        $scope.form4 = {
-            changeErrorMsg: 'This is the First Error Msg',
-            changeMsg: function () {
-                $scope.form4.changeErrorMsg = 'This is the Second Error Msg';
-            }
-        };
+            // or we can just setup directly
+            $validationProvider.setDefaultMsg({ ip: { error: 'This no ip', success: 'this ip'}});
+            $validationProvider.setExpression({ ip: /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/ });
 
-        $scope.form5 = {
-            checkValid: $validationProvider.checkValid,
-            submit: function (form) {
-                $validationProvider.validate(form);
-            },
-            reset: function (form) {
-                $validationProvider.reset(form);
-            }
-        };
+            /**
+             * Additions validation
+             */
+            $validationProvider.setExpression({
+                /**
+                 * @param value , user input
+                 * @returns {boolean} true iff valid
+                 */
+                huei: function (value) {
+                    return value === 'Huei Tan';
+                }
+            });
 
-        // Callback method
-        $scope.success = function (message) {
-            alert('Success ' + message);
-        };
+            $validationProvider.setDefaultMsg({
+                huei: {
+                    error: 'This should be Huei Tan',
+                    success: 'Thanks!'
+                }
+            });
+        }])
 
-        $scope.error = function (message) {
-            alert('Error ' + message);
-        };
-    }]);
+        // -------------------
+        // controller phase
+        // -------------------
+        .controller('index', ['$scope', '$injector', function ($scope, $injector) {
+
+            // Injector
+            var $validationProvider = $injector.get('$validation');
+
+
+            // Initial Value
+            $scope.form = {
+                requiredCallback: 'required',
+                checkValid: $validationProvider.checkValid,
+                submit: function (form) {
+                    $validationProvider.validate(form);
+                },
+                reset: function (form) {
+                    $validationProvider.reset(form);
+                }
+            };
+
+            $scope.form2 = {
+                checkValid: $validationProvider.checkValid,
+                submit: function (form) {
+                    $validationProvider.validate(form);
+                },
+                reset: function (form) {
+                    $validationProvider.reset(form);
+                }
+            };
+
+            $scope.form3 = {
+                checkValid: $validationProvider.checkValid,
+                submit: function (form) {
+                    $validationProvider.validate(form)
+                        .success($scope.success)
+                        .error($scope.error);
+                },
+                reset: function (form) {
+                    $validationProvider.reset(form);
+                }
+            };
+
+            $scope.form4 = {
+                changeErrorMsg: 'This is the First Error Msg',
+                changeMsg: function () {
+                    $scope.form4.changeErrorMsg = 'This is the Second Error Msg';
+                }
+            };
+
+            $scope.form5 = {
+                checkValid: $validationProvider.checkValid,
+                submit: function (form) {
+                    $validationProvider.validate(form);
+                },
+                reset: function (form) {
+                    $validationProvider.reset(form);
+                }
+            };
+
+            // Callback method
+            $scope.success = function (message) {
+                alert('Success ' + message);
+            };
+
+            $scope.error = function (message) {
+                alert('Error ' + message);
+            };
+        }]);
+}).call(this);
