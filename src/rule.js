5f069820d2520a2bfc4e52906d446dac5edf2d97
Huei Tan <huei90@gmail.com>
2014-07-18 00:07:35 +0800

    Fix rule required #28

--- src/rule.js
+++ src/rule.js
@@ -3,7 +3,9 @@
         .config(['$validationProvider', function ($validationProvider) {
 
             var expression = {
-                required: /^.+$/,
+                required: function (value) {
+                    return !!value;
+                },
                 url: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
                 email: /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
                 number: /^\d+$/
