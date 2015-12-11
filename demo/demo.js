37d4f87739460c80d8ff43a39bc3f48fed624034
huei90 <huei90@gmail.com>
2015-12-11 16:37:47 +0800

    Update demo page (remove bower to cdn)

--- demo/demo.js
+++ demo/demo.js
@@ -90,6 +90,7 @@
   // -------------------
   .controller('index', ['$scope', '$injector', function($scope, $injector) {
     // Injector
+
     var $validationProvider = $injector.get('$validation');
 
     // Initial Value
