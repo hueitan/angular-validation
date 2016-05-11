4679067014a46877b2effb7c328289dce8717ab5
Dmitry Dedukhin <DDedukhin@ptsecurity.com>
2016-05-11 16:31:34 +0300

    Fixed calls for validCallback/invalidCallback: tests & build

--- src/module.js
+++ src/module.js
@@ -1,6 +1,3 @@
-
-
 angular.module('validation', ['validation.provider', 'validation.directive']);
 angular.module('validation.provider', []);
 angular.module('validation.directive', ['validation.provider']);
-
