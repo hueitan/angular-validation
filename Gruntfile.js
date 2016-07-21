7ce1a2d29541f1556c44155f6fb69a3c300986f2
nmccready <nemtcan@gmail.com>
2016-07-20 20:32:16 -0400

    grunt default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,4 +112,5 @@ module.exports = function(grunt) {
   grunt.registerTask('build', ['check', 'clean', 'concat', 'uglify', 'copy']);
   grunt.registerTask('test', ['build', 'karma']);
   grunt.registerTask('cov', ['coveralls']);
+  grunt.registerTask('default', ['build', 'test']);
 };
